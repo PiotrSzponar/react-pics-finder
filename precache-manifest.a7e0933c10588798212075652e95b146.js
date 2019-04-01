f7bf9e176959ced9af5677dd1fa0ea2f88a64f78
Piotr Szponar <18piotrek06@gmail.com>
2019-04-01 15:03:23 +0200

    Updates

---
+++ precache-manifest.a7e0933c10588798212075652e95b146.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ce6d5e648073fc71bf8",
+    "url": "/react-pics-finder/static/js/runtime~main.79dc1acf.js"
+  },
+  {
+    "revision": "77bebf0c7ab24960b781",
+    "url": "/react-pics-finder/static/js/main.38df0e25.chunk.js"
+  },
+  {
+    "revision": "e053a1da17cbfeebd8e3",
+    "url": "/react-pics-finder/static/js/2.9176d4f9.chunk.js"
+  },
+  {
+    "revision": "77bebf0c7ab24960b781",
+    "url": "/react-pics-finder/static/css/main.7b28e969.chunk.css"
+  },
+  {
+    "revision": "4b795952674dce6fb3d15bfaf6804985",
+    "url": "/react-pics-finder/index.html"
+  }
+];
